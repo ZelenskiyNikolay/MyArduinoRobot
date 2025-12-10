105a894a31f54ad3dfd4124f087664a27da9cbe9
ZelenskiyNikolay <zelenskiy.nikolayy@gmail.com>
2025-12-10 21:24:01 +0200

    Surface sensor class Класс датчиков поверхности

---
+++ src/Sensors/SurfaceSensor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class SurfaceSensor {
+public:
+    SurfaceSensor(int pin);
+    void update();
+    bool GetSensorState();
+private:
+    int SensorPin;
+    bool lastState = false;
+};
