31d50d95aed39b0acf0324eb27e121b4623c4155
ZelenskiyNikolay <zelenskiy.nikolayy@gmail.com>
2026-01-19 19:14:26 +0200

    Добавлены датчики

---
+++ src/Sensors/MotionSensor.cpp
@@ -0,0 +1,20 @@
+#include "MotionSensor.h"
+#include <Arduino.h>
+
+MotionSensor::MotionSensor(int pin) : pirPin(pin)
+{
+    pinMode(pirPin, INPUT);
+    lastState = LOW;
+}
+
+void MotionSensor::update()
+{
+    bool motion = digitalRead(pirPin);
+    // Сработал впервые → отправляем событие
+    if (motion && !lastState)
+    {
+       // EventBus::push({EVENT_MOTION});
+    }
+
+    lastState = motion;
+}
