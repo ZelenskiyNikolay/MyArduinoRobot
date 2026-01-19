bca49b45ccff712c2088aa9130950cb4c821d03a
ZelenskiyNikolay <zelenskiy.nikolayy@gmail.com>
2026-01-19 18:31:59 +0200

    Добавлены состояния

---
+++ src/Core/State.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "EventBus.h"
+//#include "FSM.h"
+//#include "StateCommand.h"
+
+enum StateID {
+    STATE_START = 0,
+    STATE_CALIBRATION = 1,
+    STATE_NORMAL = 2,
+    STATE_SLEEPY = 3,
+    STATE_CLOCK  = 4
+};
+
+// enum StateCommand {
+//     CMD_NONE = 0,
+//     CMD_TO_CALIBRATION,
+//     CMD_TO_NORMAL,
+//     CMD_TO_SLEEPY,
+//     CMD_TO_ANGRY,
+//     CMD_TO_HAPPY
+// };
+
+class State {
+public:
+    virtual ~State() {} 
+
+    virtual void enter() {}
+    virtual void update(float dt) {}
+    //virtual StateCommand handleEvent(Event e) { return CMD_NONE; }
+    virtual void exit() {};
+};
